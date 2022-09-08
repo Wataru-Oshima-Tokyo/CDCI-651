df2cfa2486ea9d29df0c99b0b137917e719a998d
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-09-08 15:15:14 +0900

    added sth

--- week1-2/assignment_1.py
+++ week1-2/assignment_1.py
@@ -2,6 +2,9 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+import os
+import pandas as pd
+from  collections import Counter 
 
 # erdos_renyi graph generator
 def _erdos_renyi_graph(n, p):
@@ -55,56 +58,76 @@ def question4():
 def question5():
     n = 1000 # number of nodes
     numOfGraphs =10 # number of graphs
-    _p = 0.002 # reasonable probability 
+    # _p = 1/(n-1) # reasonable probability 
     x =[]
     y =[]
     k =[]
+    
     c =[]
-    for a in range(numOfGraphs):
-        p = np.random.uniform(0,_p)
-        G, Gcc, avgD = _erdos_renyi_graph(n,p)
-        x.append(avgD)
-        y.append(Gcc/n)
-        if avgD >1:
-            k.append("Supercritiacal regime")
-        elif avgD <1:
-            k.append("Subcritical regime")
-        else:
-            k.append("Crital point")
-        if Gcc/n ==1:
-            c.append(True)
-        else:
-            c.append(False)
-    # print(x,y)
-    plt.scatter(x,y)
+    counter =0
+    for a in range(500):
+        a *=0.05
+        if a != 0:
+            k.append(a)
+    for _k in k:
+        print(_k)
+        p = _k/(n-1)
+        _y =[]
+        for a in range(numOfGraphs):
+            G, Gcc, avgD = _erdos_renyi_graph(n,p)
+            _y.append(Gcc/n)
+        x.append(_k)
+        avg = sum(_y)/len(_y)
+        y.append(avg)
+        print("avg: ", avg)
+    print(x,y)
+    plt.plot(x,y)
+    # plt.yscale("log")
     #obtain m (slope) and b(intercept) of linear regression line
-    m, b = np.polyfit(x, y, 1)
-    line = [m*_x +b for _x in x]
-    #use red as color for regression line
-    plt.plot(x, line, color='red')
+    # m, b = np.polyfit(x, y, 1)
+    # line = [m*_x +b for _x in x]
+    # #use red as color for regression line
+    # plt.plot(x, line, color='red')
     plt.xlabel('<k>')
     plt.ylabel('(size of giant comp)/number of vertices')
     #Showing the result for each graph
-    for i in range(numOfGraphs):
-        print("The graph ", end="")
-        print(i+1)
-        print(k[i])
-        if c[i]:
-            print("connected regime\n")
-        else:
-            print("not connected regime\n")
+    # for i in range(numOfGraphs):
+    #     print("The graph ", end="")
+    #     print(i+1)
+    #     print(k[i])
+    #     if c[i]:
+    #         print("connected regime\n")
+    #     else:
+    #         print("not connected regime\n")
     plt.show()
 
+
 def question6_1():
-    # US airport networks (2010)
-    n = 1574 # number of nodes
-    E = 28236 # number of edges
-    p = E/math.comb(n,2) # probability
-    G = nx.gnp_random_graph(n,p, directed=False)
+    # Network scientist coauthorships (2019)
+    #loading the data set
+    #get the current directory
+    directory_path = os.getcwd()
+    print("My current directory is : " + directory_path)
+    filename = directory_path + "\edgelist.xlsx"
+    print("My file name is : " + filename)
+    data = pd.read_excel(filename)
+    df = pd.DataFrame(data)
+
+    print(df.max(numeric_only=True).max())
+    L = list(zip(df["source"], df["target"]))
+    N = (int)(df.max(numeric_only=True).max()+1)  # number of nodes
+    E = len(L) # number of edges
+    p = E/math.comb(N,2) # probability
+    G = nx.Graph()
+    G.add_nodes_from([i for i in range(N)])
+    G.add_edges_from(L)
+    # print(G.degree())
+
+    # G = nx.gnp_random_graph(n,p, directed=False)
     degrees = [G.degree(i) for i in G.nodes()]
-    print(degrees)
-    print(G.nodes())
-    # plot with various axes scales
+    # print(degrees)
+    # print(G.nodes())
+    # # plot with various axes scales
     plt.figure()    
     plt.subplot(221)
     plt.scatter(degrees, G.nodes())
@@ -116,7 +139,8 @@ def question6_1():
     plt.subplot(224)
     plt.scatter(degrees, G.nodes())
     plt.yscale("log")
-    plt.xlabel('Degree')
+    plt.xscale("log")
+    plt.xlabel('Log Degree')
     plt.ylabel('index of nodes')
     plt.title('log')
     #Showing the result for each graph
@@ -124,61 +148,65 @@ def question6_1():
 
 def question6_2():
     # US airport networks (2010)
-    n = 1574 # number of nodes
-    E = 28236 # number of edges
-    p = E/math.comb(n,2) # probability
-    # G = nx.gnp_random_graph(n,p, directed=False)
+    directory_path = os.getcwd()
+    print("My current directory is : " + directory_path)
+    filename = directory_path + "\edgelist.xlsx"
+    print("My file name is : " + filename)
+    data = pd.read_excel(filename)
+    df = pd.DataFrame(data)
     numOfGraphs = 10 # number of graphs
-    x =[]
-    y =[]
-    k =[]
-    c =[]
+
+
+
+    print(df.max(numeric_only=True).max())
+    L = list(zip(df["source"], df["target"]))
+    N = (int)(df.max(numeric_only=True).max()+1)  # number of nodes
+    E = len(L) # number of edges
+    p = E/math.comb(N,2) # probability
     Graphs =[]
-    #generate 10 ER graphs with n and e
-    print(p)
     for a in range(numOfGraphs):
-        G, Gcc, avgD = _erdos_renyi_graph(n,p)
-        x.append(avgD)
-        y.append(Gcc/n)
+        G, Gcc, avgD = _erdos_renyi_graph(N,p)
         Graphs.append(G)
-    degrees =[]
+        print(".", end="")
+    print("")
+    
 
     # get all the degrees for each graph
-    for i in range(len(Graphs)):
-        degrees.append([Graphs[i].degree(j) for j in Graphs[i].nodes()])
-    
-    avgDegrees =[]
+    ex = dict(Counter([G.degree(j) for j in G.nodes()] for G in Graphs))
+    degrees = list(ex.keys())
+    node = []
     #take the average of each degree in 10 graphs such as (degree[1][0] + degree[2][0]) /2 ...
-    for i in range(len(Graphs[0].nodes())):
-        total =0
-        for j in range(len(Graphs)):
-            total += degrees[j][i]
-        avgDegrees.append(total/len(Graphs))
+    for key in degrees:
+        node.append(ex[key]/(int)(df.max(numeric_only=True).max()))
 
-    # expected and average degree distribution
-    #Since a degree is equaly treated, there is not weight to consider
     Ep =0
-    for i in avgDegrees:
-        Ep += i/len(Graphs[0].nodes())
-    # Ep += [i/len(Graphs[0].nodes()) for i in avgDegrees]
+    # expected and average degree distribution
+    # Since a degree is equaly treated, there is not weight to consider
+    for i in degrees:
+        Ep += i/len(degrees)
     print("Expected and approximate degree distribution is ", Ep) 
     # plot with various axes scales
     plt.figure()    
     plt.subplot(221)
-    plt.scatter(avgDegrees, Graphs[0].nodes())
+    plt.scatter(degrees, node)
     plt.yscale("linear")
     plt.xlabel('Degree')
-    plt.ylabel('index of nodes')
-    plt.title('linear')
+    plt.ylabel('num nodes')
+    plt.title('Coauthorships Degree Distribution')
 
     plt.subplot(224)
-    plt.scatter(avgDegrees, Graphs[0].nodes())
+    plt.scatter(degrees, node)
     plt.yscale("log")
-    plt.xlabel('Degree')
-    plt.ylabel('index of nodes')
-    plt.title('log')
+    plt.xscale("log")
+    plt.xlabel('Log Degree')
+    plt.ylabel('log num nodes')
+    plt.title('log Coauthorships Degree Distribution')
     #Showing the result for each graph
     plt.show()
+
+    #Showing the result for each graph
+    plt.show()
+
 
 if __name__ == "__main__":
     question4()
