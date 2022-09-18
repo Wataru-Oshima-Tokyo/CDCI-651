ae70b07cb432d52a0e9e43b3fdd70bc5bbd2d062
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-09-19 08:06:07 +0900

    done it for week3-4

--- week3-4/assignment_2.py
+++ week3-4/assignment_2.py
@@ -1,3 +1,6 @@
+from cProfile import label
+import colorsys
+from functools import total_ordering
 import networkx as nx
 import matplotlib.pyplot as plt
 import math
@@ -5,7 +8,10 @@ import numpy as np
 import os
 import pandas as pd
 from  collections import Counter 
-# from __future__ import print_function
+import time
+from multiprocessing import Pool
+import platform
+
 
 def _stublist(degSeq):
     L =[]
@@ -109,99 +115,143 @@ def question2_a():
     plt.show()
 
 def question2_c():
+    os_name = platform.system()
+    print(os_name)
     directory_path = os.getcwd()
     print("My current directory is : " + directory_path)
-    filename = directory_path + "\Openflights_airport_network_2016.xlsx"
-    print("My file name is : " + filename)
-    data = pd.read_excel(filename)
-    df = pd.DataFrame(data)
+    if os_name =="Windows":
+        file_names =[directory_path + "\german_highway.xlsx", directory_path + "\Openflights_airport_network_2016.xlsx"]
+    else:
+        file_names =[directory_path + "/german_highway.xlsx", directory_path + "/Openflights_airport_network_2016.xlsx"]
+    for d in range(2):
+        filename = file_names[d]
+        print("My file name is : " + filename)
+        data = pd.read_excel(filename)
+        df = pd.DataFrame(data)
+        L = []
+        _max =0
+        if d ==1:
+            _max =(int)(df.max(numeric_only=True).max()+1)  
+            L = list(zip(df["source"], df["target"]))
+        else:
+            _max = (int)(len(df)+1)
+            for v in range(_max):
+                for u in range(len(df.columns)):
+                    try:
+                        if df[v][u] ==1:
+                            L.append((v,u))
+                    except:
+                        pass
+        print(_max)
+        N = _max # number of nodes
+        E = len(L) # number of edges
+        p = E/math.comb(N,2) # probability
 
-    print(df.max(numeric_only=True).max())
-    L = list(zip(df["source"], df["target"]))
-    N = (int)(df.max(numeric_only=True).max()+1)  # number of nodes
-    E = len(L) # number of edges
-    p = E/math.comb(N,2) # probability
+        #Actual graph
+        actual_G = nx.Graph()
+        actual_G.add_nodes_from([i for i in range(N)])
+        actual_G.add_edges_from(L)
 
-    #Actual graph
-    actual_G = nx.Graph()
-    actual_G.add_nodes_from([i for i in range(N)])
-    actual_G.add_edges_from(L)
-
-    #get the degree seq and distribution
-    ex = dict(Counter([actual_G.degree(j) for j in actual_G.nodes()]))
-    node = []
-    degreeSeq= list(ex.keys())
-    for key in degreeSeq:
-        node.append(ex[key]/(int)(df.max(numeric_only=True).max()))
-
-
-    path = dict(nx.all_pairs_shortest_path(actual_G))
-    print(path)
-    for i in path:
-        print(len(i))
-    # all_paths =[]
-    # for key in paths:
-    #     all_paths.append(paths[key])
-    # print(all_paths)
-
-
-    #config model
-    config_G = nx.Graph()
-    config_G = configModel([actual_G.degree(j) for j in actual_G.nodes()])
-    ex = dict(Counter([config_G.degree(j) for j in config_G.nodes()]))
-    node_config = []
-    degreeSeq_config= list(ex.keys())
-
-    for key in degreeSeq_config:
-        node_config.append(ex[key]/(int)(df.max(numeric_only=True).max()))
-
-    #randamized graph
-    randamize_G = nx.Graph()
-    randamize_G = degPresRand(config_G)
-    ex = dict(Counter([randamize_G.degree(j) for j in randamize_G.nodes()]))
-    degreeSeq_random= list(ex.keys())
-    # print(degrees)
-    node_random =[]
-    for key in degreeSeq_random:
-        node_random.append(ex[key]/(int)(df.max(numeric_only=True).max()))
-    #visualzie graphs
-    position1 = nx.circular_layout(actual_G)
-    position2 = nx.circular_layout(config_G)
-    position3 = nx.circular_layout(randamize_G)
+        #get the degree seq and distribution
+        ex = dict(Counter([actual_G.degree(j) for j in actual_G.nodes()]))
+        node = []
+        degreeSeq= list(ex.keys())
+        for key in degreeSeq:
+            node.append(ex[key]/(int)(df.max(numeric_only=True).max()))
 
 
-    # degrees = [G.degree(i) for i in G.nodes()]
-    plt.figure()    
-    plt.subplot(221)
-    plt.scatter(degreeSeq, node)
-    plt.yscale("linear")
-    plt.xlabel('Degree')
-    plt.ylabel('num nodes')
-    plt.title('Actual Graph')
-   
-    plt.subplot(222)
-    plt.scatter(degreeSeq_config, node_config)
-    plt.yscale("linear")
-    plt.xlabel('Degree')
-    plt.ylabel('num nodes')
-    plt.title('config model Graph')
+        path_actual = dict(nx.all_pairs_shortest_path_length(actual_G))
+        # print(max(path))
+        distances= list(path_actual.keys())
+        distance_actual =[0]* (max(path_actual)+1)
+        for key in distances:
+            for j in path_actual[key]:
+                distance_actual[j] +=1
 
+
+        #config model
+        config_G = nx.Graph()
+        config_G = configModel([actual_G.degree(j) for j in actual_G.nodes()])
+        ex = dict(Counter([config_G.degree(j) for j in config_G.nodes()]))
+        node_config = []
+        degreeSeq_config= list(ex.keys())
+
+        for key in degreeSeq_config:
+            node_config.append(ex[key]/(int)(df.max(numeric_only=True).max()))
+
+        path_config = dict(nx.all_pairs_shortest_path_length(config_G))
+        # print(max(path))
+        distances= list(path_config.keys())
+        distance_config =[0]* (max(path_config)+1)
+        for key in distances:
+            for j in path_config[key]:
+                distance_config[j] +=1
+
+        #randamized graph
+        randamize_G = nx.Graph()
+        randamize_G = degPresRand(config_G)
+        ex = dict(Counter([randamize_G.degree(j) for j in randamize_G.nodes()]))
+        degreeSeq_random= list(ex.keys())
+        # print(degrees)
+        node_random =[]
+        for key in degreeSeq_random:
+            node_random.append(ex[key]/(int)(df.max(numeric_only=True).max()))
+
+        
+        path_radnom = dict(nx.all_pairs_shortest_path_length(randamize_G))
+        # print(max(path))
+        distances= list(path_radnom.keys())
+        distance_random =[0]* (max(path_radnom)+1)
+        for key in distances:
+            for j in path_radnom[key]:
+                distance_random[j] +=1
+
+        #visualzie graphs
+        position1 = nx.circular_layout(actual_G)
+        position2 = nx.circular_layout(config_G)
+        position3 = nx.circular_layout(randamize_G)
+
+
+        # degrees = [G.degree(i) for i in G.nodes()]
+        plt.figure()    
+        plt.subplot(221)
+        plt.scatter(degreeSeq, node,color="red",label='Actual Grpah',alpha=0.3, edgecolors='none')
+        plt.scatter(degreeSeq_config, node_config, color="blue", label="Config Model",alpha=0.3, edgecolors='none')
+        plt.scatter(degreeSeq_random, node_random, color="green", label="Random Model",alpha=0.3, edgecolors='none')
+        plt.yscale("log")
+        plt.xlabel('Degree')
+        plt.ylabel('num nodes')
+        plt.title('Degree distribution')
+        plt.legend()
+        plt.subplot(224)
+    
+        plt.yscale("linear")
+        plt.xlabel('Distance')
+        plt.ylabel('num edges')
+        plt.scatter(distance_actual, [i for i in range(len(distance_actual))], color="red",label='Actual Grpah',alpha=0.3, edgecolors='none')
+        plt.scatter(distance_config, [i for i in range(len(distance_config))], color="blue",label='Config Model',alpha=0.3, edgecolors='none')
+        plt.scatter(distance_random, [i for i in range(len(distance_random))], color="green",label='Random Model',alpha=0.3, edgecolors='none')
+        plt.title('Distance distribution')
+        plt.legend()
+        plt.show()
     
  
-    plt.subplot(223)
-    plt.scatter(degreeSeq_random, node_random)
-    plt.yscale("linear")
-    plt.xlabel('Degree')
-    plt.ylabel('num nodes')
-    plt.title('randamized Graph')
+    # # plt.subplot(223)
+    
+    # plt.yscale("linear")
+    # plt.xlabel('Degree')
+    # plt.ylabel('num nodes')
+    # plt.title('randamized Graph')
 
-    plt.show()
 
 def preferntal_attachment(G,m0,t,a,i,tk):
     Ki =G.degree(i)
     if tk ==0:
         tk =1
+    if Ki ==0:
+        Ki =1
     p = a*(1/(m0+t-1))+(1-a)*(Ki/tk)
+    
     return p
 
 def preferential_attachment_mixture_model(n,m0,m,a):
@@ -220,91 +270,61 @@ def preferential_attachment_mixture_model(n,m0,m,a):
     # from here we use the probability of preferential attachment
     t= 1
     preferential_edges =[]
-    total_k = G.degree(1)
+    total_k =0
+    for i in G.nodes():
+        total_k += G.degree(i) 
+
     for i in range(m0,n):
-        G.add_node(i)
-        for j in range(0,i-1):
-            if np.random.rand() < preferntal_attachment(G,m0,t,a,j,total_k):
-                preferential_edges.append((i,j))
+        G.add_node(i) # addding a node
+        for j in range(0,i-1): #decide whcih node should be conneceted to node i
+            p = preferntal_attachment(G,m0,t,a,j,total_k) # get the prob for this time
+            if np.random.rand() < p:
+                # preferential_edges.append((i,j))
+                G.add_edge(i,j)
+
         total_k += G.degree(m0+t-1)
+        # print("total_k:", total_k)
         t+=1 # increment the time
         print("Elapsed time is ", t)
+        # print("p is ", p)
     #get the probability to add an egge from v to i
-    G.add_edges_from(preferential_edges)
+    # G.add_edges_from(preferential_edges)
     return G
 
 
-def example():
-    directory_path = os.getcwd()
-    print("My current directory is : " + directory_path)
-    filename = directory_path + "\Openflights_airport_network_2016.xlsx"
-    print("My file name is : " + filename)
-    data = pd.read_excel(filename)
-    df = pd.DataFrame(data)
 
-    print(df.max(numeric_only=True).max())
-    L = list(zip(df["source"], df["target"]))
-    N = (int)(df.max(numeric_only=True).max()+1)  # number of nodes
-    E = len(L) # number of edges
-    p = E/math.comb(N,2) # probability
-
-    #Actual graph
-    actual_G = nx.Graph()
-    actual_G.add_nodes_from([i for i in range(N)])
-    actual_G.add_edges_from(L)
-
-    #get the degree seq and distribution
-    ex = dict(Counter([actual_G.degree(j) for j in actual_G.nodes()]))
-    node = []
-    degreeSeq= list(ex.keys())
-    for key in degreeSeq:
-        node.append(ex[key]/(int)(df.max(numeric_only=True).max()))
-
-
-    path = dict(nx.all_pairs_shortest_path(actual_G))
-    # print(path)
-    keys = list(path.keys())
-    # print(keys)
-    ditance_dist = []
-    edges =[]
-    for i in keys:
-        ditance_dist.append(len(path[i]))
-        edges.append(i)
-    
-    plt.scatter(edges, ditance_dist)
-    plt.yscale("linear")
-    plt.xlabel('indx of edges')
-    plt.ylabel('distance')
-    plt.title('actual Graph')
-
-    plt.show()
-
-
-if __name__ == "__main__":
-    # question2_c()
-    # example()
+def quesiton3():
     n = int(math.pow(10,4))
     arpha = [0,1/2,1]
+    colors = ["red","blue", "green"]
+    titles = ["a=0","a=0.5","a=1"]
     m0,m, =4,4
     subplotNum =220
-    plt.figure()
-    for i in range(len(arpha)):
-        G1 = preferential_attachment_mixture_model(n,m0,m,arpha[i])
-        ex = dict(Counter([G1.degree(j) for j in G1.nodes()]))
+    pool = Pool() # make a multiprocessing 
+    Graphs = pool.starmap(preferential_attachment_mixture_model, [(n,m0,m,arpha[0]),(n,m0,m,arpha[1]),(n,m0,m,arpha[2])]) # make a graph simultenously
+    pool.close()
+
+    for i in range(len(Graphs)):
+        ex = dict(Counter([Graphs[i].degree(j) for j in Graphs[i].nodes()])) #ge the degree counts 
         node = []
-        degreeSeq= list(ex.keys())
+        degreeSeq= list(ex.keys()) # get the degree distribution
         for key in degreeSeq:
             node.append(ex[key]/n)
-        subplotNum += 1
-        title = "arpha: " + str(arpha[i])
-        plt.subplot(subplotNum)
-        plt.scatter(degreeSeq, node)
-        plt.yscale("linear")
-        plt.xlabel('Degree')
-        plt.ylabel('num nodes')
-        plt.title(title)
-
+        plt.scatter(degreeSeq, node, color=colors[i], label=titles[i],alpha=0.3, edgecolors='none')
+    
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.xlabel('Degree')
+    plt.ylabel('num nodes')
+    plt.title("pref_attach_mixture")
+    plt.legend()
     plt.show()
+if __name__ == "__main__":
+    question2_c()
+    quesiton3()    os_name = os.platform.system()
+    print(os_name)
+
+
     
         
  
