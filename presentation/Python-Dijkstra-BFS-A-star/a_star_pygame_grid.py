fdd0f0633a989f3ae541c0c0b793be5bcbad6794
Wataru <wataruoshima@my.smccd.edu>
2022-10-14 10:11:52 -0700

    modified some files

--- presentation/Python-Dijkstra-BFS-A-star/a_star_pygame_grid.py
+++ presentation/Python-Dijkstra-BFS-A-star/a_star_pygame_grid.py
@@ -17,6 +17,25 @@ def get_neighbours(x, y):
     ways = [-1, 0], [0, -1], [1, 0], [0, 1], [-1, -1], [1, -1], [1, 1], [-1, 1] #8 ways to go
     return [(grid[y + dy][x + dx], (x + dx, y + dy)) for dx, dy in ways if check_neighbour(x + dx, y + dy)]
 
+
+def get_click_mouse_pos():
+    x, y = pg.mouse.get_pos()
+    grid_x, grid_y = x // TILE, y // TILE
+    pg.draw.circle(sc, pg.Color('red'), *get_circle(grid_x, grid_y))
+    click = pg.mouse.get_pressed()
+    return (grid_x, grid_y) if click[0] else False
+
+
+def get_rect(x, y):
+    return x * TILE + 1, y * TILE + 1, TILE - 2, TILE - 2
+
+
+def heuristic(a, b):
+   return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def dynamic_heuristic(a,b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
 def make_grid(random_,t):
     global grid
     grid = [[]]
@@ -41,48 +60,45 @@ def make_grid(random_,t):
                 #third triangle
                 elif math.sqrt(pow((row -t),2)+ pow((col-t),2))<8:
                     grid[row].append(100) 
+                elif math.sqrt(pow((row*2 -t),2)+ pow((col-t),2))<10:
+                    grid[row].append(100) 
+                elif math.sqrt(pow((row -t),2)+ pow(((cols-col)-t),2))<10:
+                        grid[row].append(100) 
                 else:
                     grid[row].append(0)
 
 
-         
+def dijkstra_dstar(start_,goal_, graph):
+    global grid,start_time,start
+    queue = [] #this is the open List
+    heappush(queue, (0, start_))  # added the start_ point to the open List
+    cost_visited = {start_: 0} #make a closed cost list with the start_ point
+    visited = {start_: None} #make a closed node list with the start_ point
+    while queue:
+        cur_cost, cur_node = heappop(queue) #get the node with the minimum cost from the open List
+        if cur_node == goal_: ## if the point is the goal_ then break it
+            break
+
+        neighbours = graph[cur_node] #get the neibours of the current node
+        for neighbour in neighbours: #for each neighbor, the below operation will be applied
+            neigh_cost, neigh_node = neighbour # get the cost and coordinate of the neigbor
+            new_cost = cost_visited[cur_node] + neigh_cost #calculate the new cost with the cost of current node + the neighbour's cost
+
+            #if the neighbor has not been visited yet and the new cost is less than the cost to go to the goal_ from the neighbour
+            if neigh_node not in cost_visited or new_cost < cost_visited[neigh_node]: 
+                # calculate g(n) + h(n) which is the cost of node itself + the linear distance from this neighbour to the goal_
+                priority = new_cost + dynamic_heuristic(neigh_node, goal_) 
+                heappush(queue, (priority, neigh_node)) # add the node and new cost to the openList 
+                cost_visited[neigh_node] = new_cost #update the cost of the node in the closed cost list
+                visited[neigh_node] = cur_node #update the node to visited node in the closed visited list
+    return visited
 
 
-def get_click_mouse_pos():
-    x, y = pg.mouse.get_pos()
-    grid_x, grid_y = x // TILE, y // TILE
-    pg.draw.circle(sc, pg.Color('red'), *get_circle(grid_x, grid_y))
-    click = pg.mouse.get_pressed()
-    return (grid_x, grid_y) if click[0] else False
-
-
-def get_rect(x, y):
-    return x * TILE + 1, y * TILE + 1, TILE - 2, TILE - 2
-
-
-def heuristic(a, b):
-   return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
 
 
-def dynamic_map(start, goal):
-    create_time_start = time.time()
-    global grid, graph
-    grid=[[]]
-    for row in range(rows):
-        grid.append([])
-        for col in range(cols):
-            if random() < 0.2 and (col, row) != start and (col, row) != goal:
-                grid[row].append(100)
-            else:
-                grid[row].append(0)
-
-    create_time_end = time.time()
-    return (create_time_end-create_time_start) #erase the time to create a new map
-
-
-def bfs(start, goal, grpah, dynamic):
+def bfs(start, goal, grpah):
     queue = []
     heappush(queue, (0, start))
     cost_visited = {start: 0} 
@@ -102,38 +118,21 @@ def bfs(start, goal, grpah, dynamic):
                 heappush(queue, (priority, neigh_node))
                 cost_visited[neigh_node] = new_cost
                 visited[neigh_node] = cur_node
-        if dynamic:
-            start_time+=dynamic_map(start, goal)
     return visited
 
 
 
-def dijkstra_astar(start_, goal_, graph, dynamic):
+def dijkstra_astar(start_, goal_, graph):
     global grid,start_time,start
     queue = [] #this is the open List
     heappush(queue, (0, start_))  # added the start_ point to the open List
     cost_visited = {start_: 0} #make a closed cost list with the start_ point
     visited = {start_: None} #make a closed node list with the start_ point
-    if not dynamic:
-        while queue:
-            cur_cost, cur_node = heappop(queue) #get the node with the minimum cost from the open List
-            if cur_node == goal_: ## if the point is the goal_ then break it
-                break
-
-            neighbours = graph[cur_node] #get the neibours of the current node
-            for neighbour in neighbours: #for each neighbor, the below operation will be applied
-                neigh_cost, neigh_node = neighbour # get the cost and coordinate of the neigbor
-                new_cost = cost_visited[cur_node] + neigh_cost #calculate the new cost with the cost of current node + the neighbour's cost
-
-                #if the neighbor has not been visited yet and the new cost is less than the cost to go to the goal_ from the neighbour
-                if neigh_node not in cost_visited or new_cost < cost_visited[neigh_node]: 
-                    # calculate g(n) + h(n) which is the cost of node itself + the linear distance from this neighbour to the goal_
-                    priority = new_cost + heuristic(neigh_node, goal_) 
-                    heappush(queue, (priority, neigh_node)) # add the node and new cost to the openList 
-                    cost_visited[neigh_node] = new_cost #update the cost of the node in the closed cost list
-                    visited[neigh_node] = cur_node #update the node to visited node in the closed visited list
-    else:
+    while queue:
         cur_cost, cur_node = heappop(queue) #get the node with the minimum cost from the open List
+        if cur_node == goal_: ## if the point is the goal_ then break it
+            break
+
         neighbours = graph[cur_node] #get the neibours of the current node
         for neighbour in neighbours: #for each neighbor, the below operation will be applied
             neigh_cost, neigh_node = neighbour # get the cost and coordinate of the neigbor
@@ -146,25 +145,21 @@ def dijkstra_astar(start_, goal_, graph, dynamic):
                 heappush(queue, (priority, neigh_node)) # add the node and new cost to the openList 
                 cost_visited[neigh_node] = new_cost #update the cost of the node in the closed cost list
                 visited[neigh_node] = cur_node #update the node to visited node in the closed visited list
-                start = cur_node
     return visited
 
 if __name__ =="__main__":
     
-    if len(sys.argv) == 3:
-        cols, rows = int(sys.argv[1]), int(sys.argv[2])
+    if len(sys.argv) == 5:
+        cols, rows, dynamic, astar = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4])
     else:
-        cols, rows = 100, 100
+        cols, rows, dynamic, astar = 100, 100,0,1
     TILE = 5
-
+    time_stamp = 3
     pg.init()
     sc = pg.display.set_mode([cols * TILE, rows * TILE])
     clock = pg.time.Clock()
-    # set grid
-
-    # grid = [[100 if random() < 0.2 and (row !=0 and col !=7) else 0 for col in range(cols)] for row in range(rows)]
     grid =[[]]
-    make_grid(random_=False,t=1)
+    make_grid(random_=False,t=20)
 
     # adjacency dict
     graph = {}
@@ -178,48 +173,60 @@ if __name__ =="__main__":
     heappush(queue, (0, start))
     visited = {start: None}
     start_time = time.time()
-    
+    total_time = 0.0
     t_=0
     while True:
         # fill screen
-        if t_>rows:
+        if t_>rows*time_stamp:
             t_=1
         else:
             t_+=1
-        if start ==goal:
-            start = (0, 7)
+        if start == (0,7):
+            # start = (0, 7)
+            print("The time to reach the goal  is ", total_time)
+            total_time = 0.0
         sc.fill(pg.Color('black'))
         [[pg.draw.rect(sc, pg.Color('darkorange'), get_rect(x, y), border_radius=TILE // 5)
         for x, col in enumerate(row) if col] for y, row in enumerate(grid)]
         # bfs, get path to mouse click
         pg.draw.circle(sc, pg.Color('red'), *get_circle(goal[0],goal[1]))
-        make_grid(random_=False,t=t_)
-        # adjacency dict
-        graph = {}
-        for y, row in enumerate(grid):
-            for x, col in enumerate(row):
-                graph[(x, y)] = graph.get((x, y), []) + get_neighbours(x, y)
+        if dynamic:
+            make_grid(random_=False,t=t_//time_stamp)
+            # adjacency dict
+            graph = {}
+            for y, row in enumerate(grid):
+                for x, col in enumerate(row):
+                    graph[(x, y)] = graph.get((x, y), []) + get_neighbours(x, y)
         mouse_pos = get_click_mouse_pos() 
         if mouse_pos:
             goal = mouse_pos
         start_time = time.time()
-        visited = dijkstra_astar(start, goal, graph, dynamic=True)
-        # visited = bfs(start, goal, graph,dynamic=False)
+        if astar:
+            visited = dijkstra_astar(start, goal, graph)
+        else:
+            visited = bfs(start, goal, graph)
         # goal = mouse_pos
         end_time = time.time()
-        print("The time to find a path is ", (end_time-start_time))
-
+        total_time += (end_time-start_time)
+        # print("The time to find a path is ", (end_time-start_time))
+        
         # draw path
         path_head, path_segment = goal, goal
+        temp = (0,7)
         while path_segment and path_segment in visited:
             pg.draw.rect(sc, pg.Color('black'), get_rect(path_segment[0], path_segment[1]))
             pg.draw.circle(sc, pg.Color('blue'), *get_circle(*path_segment))
-            path_segment = visited[path_segment]
-        # if (path_segment):
+            path_segment =visited[path_segment]  
+            if path_segment != None:
+                if (path_segment == start):
+                    pass
+                else:    
+                    temp = path_segment
+        start = temp
+        # print(start)
         pg.draw.circle(sc, pg.Color('green'), *get_circle(*start))
         pg.draw.circle(sc, pg.Color('magenta'), *get_circle(*path_head))
-            # time.sleep(2)
         # pygame necessary lines
         [exit() for event in pg.event.get() if event.type == pg.QUIT]
         pg.display.flip()
-        clock.tick(10)
+        clock.tick(60)
